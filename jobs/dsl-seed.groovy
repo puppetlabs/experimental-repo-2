335325a1e41cdb8efc0acb5552bc7b79726b2693
Wayne Warren <waynr+launchpad@sdf.org>
2016-07-07 16:18:23 -0700

    (QENG-3896) Add JobDSL seed script.

---
+++ jobs/dsl-seed.groovy
@@ -0,0 +1,15 @@
+workflowJob('experimental_experimental-repo-2_workflow') {
+  scm {
+    git('git@github.com:puppetlabs/experimental-repo-2.git')
+  }
+  triggers {
+    scm('H/5 * * * *')
+  }
+  definition {
+    cpsSCm {
+      scm {
+        git('git@github.com:puppetlabs/experimental-repo-2.git')
+      }
+    }
+  }
+}
