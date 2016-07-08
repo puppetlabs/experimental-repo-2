28de0ad46225c7bff956144494d7b81999e02a14
Wayne Warren <waynr+launchpad@sdf.org>
2016-07-08 12:35:24 -0700

    (QENG-3954) Begin parameterizing job creation.

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,16 +1,27 @@
-pipelineJob('experimental_experimental-repo-2_workflow') {
-  scm {
-    git('git@github.com:puppetlabs/experimental-repo-2.git')
-  }
-  triggers {
-    scm('H/5 * * * *')
-  }
-  definition {
-    cpsScm {
-      scm {
-        git('git@github.com:puppetlabs/experimental-repo-2.git')
+def value_stream = "experimental"
+def group_name = "puppetlabs"
+def project_name = "experimental-repo-2"
+def branch_api = new URL("https://api.github.com/repos/${group_name}/${project_name}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branch_api.newReader())
+
+branches.each {
+  def branch_name = it.name
+
+  pipelineJob('${value_stream}_${project}_workflow') {
+    scm {
+      git('git@github.com:${group_name}/${project_name}.git')
+    }
+    triggers {
+      upstream('${value_stream}_experimental-repo-1_workflow_${branch_name}')
+      scm('H/5 * * * *')
+    }
+    definition {
+      cpsScm {
+        scm {
+          git('git@github.com:${group_name}/${project_name}.git')
+        }
+        scriptPath('Jenkinsfile')
       }
-      scriptPath('Jenkinsfile')
     }
   }
 }
